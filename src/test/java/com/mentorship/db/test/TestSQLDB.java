d824a6836063ce407b4b85ac57c27319a9dce43a
pizhanska <pizhanska29@gmail.com>
2017-05-30 00:41:05 +0300

    add first test

--- src/test/java/com/mentorship/db/test/TestSQLDB.java
+++ src/test/java/com/mentorship/db/test/TestSQLDB.java
@@ -1,10 +1,10 @@
 package com.mentorship.db.test;
 
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * Created by Uliana Pizhanska on 30/05/2017.
@@ -12,10 +12,11 @@ import java.sql.Statement;
 public class TestSQLDB {
     private Connection connection;
     private Statement statement ;
-    private final String DB_URL = "jdbc:mysql://localhost:3310/user";
+    private final String DB_URL = "jdbc:mysql://localhost:3306/sakila?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     private final String DB_USER = "root";
     private final String PASSWORD = "12345trewq";
     private final String DB_CLASS = "com.mysql.jdbc.Driver";
+    private ResultSet resultSet;
 
     @BeforeClass
     public void createConnection(){
@@ -27,6 +28,34 @@ public class TestSQLDB {
         catch (Exception e) {
             e.printStackTrace();
             System.out.println("Kind of exception found");
+        }
+    }
+
+
+    @Test
+    public void readAll(){
+        try {
+            resultSet = statement.executeQuery("select * from actor");
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(1));
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @AfterClass
+    public void tearDown(){
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("Can't close connection");
         }
     }
 }
