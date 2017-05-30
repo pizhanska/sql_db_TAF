f5b8a0900ac7c129fff54524b207f4f87af93b02
pizhanska <pizhanska29@gmail.com>
2017-05-30 23:39:49 +0300

    add first test

--- src/test/java/com/mentorship/db/test/TestSQLDB.java
+++ src/test/java/com/mentorship/db/test/TestSQLDB.java
@@ -1,29 +1,30 @@
 package com.mentorship.db.test;
 
+import com.mentorship.db.operations.CRUDImpl;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import static com.mentorship.db.utilities.UtilityConstants.*;
 
 import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 /**
  * Created by Uliana Pizhanska on 30/05/2017.
  */
 public class TestSQLDB {
-    private Connection connection;
-    private Statement statement ;
-    private final String DB_URL = "jdbc:mysql://localhost:3306/sakila?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    private final String DB_USER = "root";
-    private final String PASSWORD = "12345trewq";
-    private final String DB_CLASS = "com.mysql.jdbc.Driver";
-    private ResultSet resultSet;
+    private Connection connection = null;
+    private ResultSet resultSet = null;
+    private CRUDImpl crud ;
 
     @BeforeClass
     public void createConnection(){
         try {
             Class.forName(DB_CLASS).newInstance();
             connection = DriverManager.getConnection(DB_URL, DB_USER, PASSWORD);
-            statement = connection.createStatement();
+            crud = new CRUDImpl();
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -34,16 +35,27 @@ public class TestSQLDB {
 
     @Test
     public void readAll(){
+        resultSet = crud.selectRecords(connection,SELECT_ALL);
+        Assert.assertNotNull(resultSet);
         try {
-            resultSet = statement.executeQuery("select * from actor");
+            Assert.assertEquals(resultSet.getMetaData().getColumnCount(), 4);
+            Assert.assertEquals("actor_id",resultSet.getMetaData().getColumnName(1));
+            Assert.assertEquals("first_name",resultSet.getMetaData().getColumnName(2));
+            Assert.assertEquals("last_name",resultSet.getMetaData().getColumnName(3));
+            Assert.assertEquals("last_update",resultSet.getMetaData().getColumnName(4));
+
             while (resultSet.next()){
-                System.out.println(resultSet.getString(1));
+                System.out.println(resultSet.getString(1)+ " " +
+                        resultSet.getString(2) + " " +
+                        resultSet.getString(3) + " " +
+                        resultSet.getString(4) + "\n");
             }
         }
-        catch (SQLException e) {
+        catch (SQLException e){
             e.printStackTrace();
         }
     }
+
 
 
     @AfterClass
