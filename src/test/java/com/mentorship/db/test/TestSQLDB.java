9aa786a41a07794546c65d84096915772d2a1006
pizhanska <pizhanska29@gmail.com>
2017-05-31 01:29:10 +0300

    add tests for deleting, updating, inserting

--- src/test/java/com/mentorship/db/test/TestSQLDB.java
+++ src/test/java/com/mentorship/db/test/TestSQLDB.java
@@ -33,7 +33,7 @@ public class TestSQLDB {
     }
 
 
-    @Test
+    @Test(priority = 1)
     public void readAll(){
         resultSet = crud.selectRecords(connection,SELECT_ALL);
         Assert.assertNotNull(resultSet);
@@ -56,6 +56,65 @@ public class TestSQLDB {
         }
     }
 
+
+    @Test(priority = 2)
+    public void readById(){
+        resultSet = crud.selectRecords(connection, SELECT_BY_ID, 301);
+        Assert.assertNotNull(resultSet);
+        try {
+            Assert.assertEquals(resultSet.getMetaData().getColumnCount(), 4);
+            Assert.assertEquals("actor_id",resultSet.getString(1));
+            Assert.assertEquals("first_name",resultSet.getString(2));
+            Assert.assertEquals("last_name",resultSet.getString(3));
+            Assert.assertEquals("last_update",resultSet.getString(4));
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(1)+ " " +
+                        resultSet.getString(2) + " " +
+                        resultSet.getString(3) + " " +
+                        resultSet.getString(4) + "\n");
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test(priority = 3)
+    public void insertActor(){
+        int result = crud.insertRecords(connection,INSERT_ACTOR,333, "volodymyr","demchyk");
+        Assert.assertEquals(result,1);
+        Assert.assertNotNull(crud.selectRecords(connection,SELECT_BY_ID, 333));
+    }
+
+
+    @Test(priority = 4)
+    public void updateActor(){
+        int result = crud.updateRecord(connection,UPDATE_ACTOR,"volodymyr","demchyk1111", 333);
+        Assert.assertEquals(result,1);
+        resultSet = crud.selectRecords(connection, SELECT_BY_ID, 333);
+        try {
+            resultSet.next();
+            Assert.assertEquals(resultSet.getString(3), "demchyk1111");
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test(priority = 5)
+    public void deleteActor(){
+        int result = crud.deleteRecord(connection,DELETE_ACTOR, 333);
+        Assert.assertEquals(result,1);
+        try {
+          Assert.assertEquals(crud.selectRecords(connection, SELECT_BY_ID, 333).next(), false);
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+
+    }
 
 
     @AfterClass
