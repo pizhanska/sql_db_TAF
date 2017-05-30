00505c3683aa1bdf1323476f5a58d28d1f44adaf
pizhanska <pizhanska29@gmail.com>
2017-05-31 01:27:51 +0300

    add CRUD operations

--- src/main/java/com/mentorship/db/operations/CRUDImpl.java
+++ src/main/java/com/mentorship/db/operations/CRUDImpl.java
@@ -38,10 +38,60 @@ public class CRUDImpl {
         return resultSet;
     }
 
-    public  java.sql.Date getCurrentTimeStamp() {
+    public static java.sql.Date getCurrentTimeStamp() {
 
         java.util.Date today = new java.util.Date();
         return new java.sql.Date(today.getTime());
 
+    }
+
+    public int insertRecords(Connection connection, String query, int id, String firstName, String lastName ){
+        int result = 0;
+        try {
+            statement = connection.prepareStatement(query);
+            statement.setInt(1,id);
+            statement.setString(2,firstName);
+            statement.setString(3,lastName);
+            statement.setDate(4,getCurrentTimeStamp());
+            result = statement.executeUpdate();
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+    public int updateRecord(Connection connection, String query, String firstName, String lastName,int id ){
+        int result = 0;
+        try {
+            statement = connection.prepareStatement(query);
+            statement.setString(1,firstName);
+            statement.setString(2,lastName);
+            statement.setDate(3,getCurrentTimeStamp());
+            statement.setInt(4,id);
+            result = statement.executeUpdate();
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public int deleteRecord(Connection connection, String query,int id ){
+        int result = 0;
+        try {
+            statement = connection.prepareStatement(query);
+            statement.setInt(1,id);
+            result = statement.executeUpdate();
+
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
