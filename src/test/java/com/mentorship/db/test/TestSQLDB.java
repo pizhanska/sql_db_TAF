f09c33fae2b6babb71ef14da6f9c2a87c34f041a
pizhanska <pizhanska29@gmail.com>
2017-06-14 01:00:12 +0300

    add tests

--- src/test/java/com/mentorship/db/test/TestSQLDB.java
+++ src/test/java/com/mentorship/db/test/TestSQLDB.java
@@ -1,6 +1,7 @@
 package com.mentorship.db.test;
 
-import com.mentorship.db.operations.CRUDImpl;
+import com.mentorship.db.model.ActorObj;
+import com.mentorship.db.operations.ActorDAO;import com.mentorship.db.utilities.ReadWriteObjFile;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -8,8 +9,8 @@ import org.testng.annotations.Test;
 import static com.mentorship.db.utilities.UtilityConstants.*;
 
 import java.sql.*;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Uliana Pizhanska on 30/05/2017.
@@ -17,14 +18,14 @@ import java.text.SimpleDateFormat;
 public class TestSQLDB {
     private Connection connection = null;
     private ResultSet resultSet = null;
-    private CRUDImpl crud ;
+    private ActorDAO actorDAO = null;
+    private ActorObj actorObj = null;
 
     @BeforeClass
     public void createConnection(){
         try {
-            Class.forName(DB_CLASS).newInstance();
-            connection = DriverManager.getConnection(DB_URL, DB_USER, PASSWORD);
-            crud = new CRUDImpl();
+            actorDAO = new ActorDAO();
+            connection = actorDAO.returnConnectionInPool();
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -35,78 +36,44 @@ public class TestSQLDB {
 
     @Test(priority = 1)
     public void readAll(){
-        resultSet = crud.selectRecords(connection,SELECT_ALL);
-        Assert.assertNotNull(resultSet);
-        try {
-            Assert.assertEquals(resultSet.getMetaData().getColumnCount(), 4);
-            Assert.assertEquals(resultSet.getMetaData().getColumnName(1),"actor_id");
-            Assert.assertEquals(resultSet.getMetaData().getColumnName(2),"first_name");
-            Assert.assertEquals(resultSet.getMetaData().getColumnName(3),"last_name");
-            Assert.assertEquals(resultSet.getMetaData().getColumnName(4),"last_update");
-
-            while (resultSet.next()){
-                System.out.println(resultSet.getString(1)+ " " +
-                        resultSet.getString(2) + " " +
-                        resultSet.getString(3) + " " +
-                        resultSet.getString(4) + "\n");
-            }
-        }
-        catch (SQLException e){
-            e.printStackTrace();
+        List<ActorObj> objs = new ArrayList() ;
+        objs = actorDAO.getAll(connection);
+        Assert.assertNotNull(objs);
+        for(ActorObj actorObj : objs){
+            System.out.println(actorObj.toString());
         }
     }
 
 
     @Test(priority = 2)
     public void readById(){
-        resultSet = crud.selectRecords(connection, SELECT_BY_ID, 301);
-        Assert.assertNotNull(resultSet);
-        try {
-            Assert.assertEquals(resultSet.getMetaData().getColumnCount(), 4);
-            resultSet.next();
-            Assert.assertEquals(resultSet.getString(1),"301");
-            Assert.assertEquals(resultSet.getString(2),"uliana");
-            Assert.assertEquals(resultSet.getString(3),"pizhanska");
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
+        ActorObj obj= new ActorObj();
+        obj = actorDAO.getEntityById(301,connection);
+        Assert.assertNotNull(obj);
+        Assert.assertEquals(obj.getActorId() ,301);
     }
 
 
     @Test(priority = 3)
     public void insertActor(){
-        int result = crud.insertRecords(connection,INSERT_ACTOR,333, "volodymyr","demchyk");
-        Assert.assertEquals(result,1);
-        Assert.assertNotNull(crud.selectRecords(connection,SELECT_BY_ID, 333));
+        ActorObj obj = new ActorObj(545,"volodymyr", "demmmchyk");
+        int res = actorDAO.create(obj,connection);
+        Assert.assertEquals(res,1);
+        Assert.assertNotNull(actorDAO.getEntityById(545,connection));
     }
 
 
     @Test(priority = 4)
     public void updateActor(){
-        int result = crud.updateRecord(connection,UPDATE_ACTOR,"volodymyr","demchyk1111", 333);
+        ActorObj obj = new ActorObj(545, "volodymyr", "besh");
+        int result = actorDAO.update(obj,545,connection);
         Assert.assertEquals(result,1);
-        resultSet = crud.selectRecords(connection, SELECT_BY_ID, 333);
-        try {
-            resultSet.next();
-            Assert.assertEquals(resultSet.getString(3), "demchyk1111");
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
     }
 
     @Test(priority = 5)
     public void deleteActor(){
-        int result = crud.deleteRecord(connection,DELETE_ACTOR, 333);
+        int result = actorDAO.delete(545,connection);
         Assert.assertEquals(result,1);
-        try {
-          Assert.assertEquals(crud.selectRecords(connection, SELECT_BY_ID, 333).next(), false);
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
-
     }
 
 
